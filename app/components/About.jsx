006a0218476eb2eb56642370b26d963a92d01fcc
Sonika Maharjan <sonikamaharjan95@gmail.com>
2017-06-12 12:36:15 +0545

    committed again

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,27 @@
 var React=require('react');
 
-var About= (props)=> {
+var About= ()=> {
     return(
-        <h3>About Component</h3>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>
+                This is a weather application built on React.
+                I have built this app for The Complete React Web App Developer Course.
+            </p>
+            <p>
+                Here are some of the tools I used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>we
+                     - This was theJavaScript framework used.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">Open Weather Map</a>
+                     - I used Open Weather Map to search for weather data by city name.
+                </li>
+            </ul>
+        </div>
     );
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React=require('react');
+var {Link}=require('react-router')
 
-var Examples=(props)=>{
+var Examples=()=>{
     return(
         <div>
-            <h3>Examples Component</h3>
-            <p>welcome to examples pageh</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out</p>
+            <ol>
+                <li>
+                    <link to="/?location=Kathmandu">Kathmandu, Nepal</link>
+                </li>
+                <li>
+                    <Link to="/?location=Rio">Rio, Brazil</Link>
+                </li>
+            </ol>
         </div>
     );
 }
